41953fc4e3a2d3f02e8c5bfd50785c6d5096dd98
lodjdheheu <lodjdheheu@gmail.com>
2022-02-04 19:53:33 +0300

    Adding POJO's. Closes #2.

--- src/main/java/org/springframework/sfgpetclinic/model/Owner.java
+++ src/main/java/org/springframework/sfgpetclinic/model/Owner.java
@@ -1,8 +1,14 @@
 package org.springframework.sfgpetclinic.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * Created by Polik on 2/4/2022
  */
+@Getter @Setter @NoArgsConstructor @ToString
 public class Owner extends Person {
 
 }
