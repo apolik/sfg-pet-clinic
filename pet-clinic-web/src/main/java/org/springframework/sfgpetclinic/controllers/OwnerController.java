1a037157cd986f0d66958aa99e7e757e322049fb
lodjdheheu <lodjdheheu@gmail.com>
2022-02-12 21:37:46 +0300

    Adding owners index page and controller, closes #10

---
+++ pet-clinic-web/src/main/java/org/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,16 @@
+package org.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Polik on 2/12/2022
+ */
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners() {
+        return "owners/index";
+    }
+}
