e9458a2c12f0570884fac504cf15b4ec787553ba
lodjdheheu <lodjdheheu@gmail.com>
2022-02-26 14:48:28 +0300

    added layout to index page, closes #16

--- pet-clinic-web/src/main/java/org/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/org/springframework/sfgpetclinic/controllers/IndexController.java
@@ -9,8 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping({"/", "", "index.html", "index"})
+    @RequestMapping({"/", ""})
     public String index() {
+
         return "index";
     }
 }
