fb2094b5e44263789ee0d294ae974c62538c2cfc
lodjdheheu <lodjdheheu@gmail.com>
2022-03-02 20:10:11 +0300

    Added Specialties map Impl, closes #30

---
+++ pet-clinic-data/src/main/java/org/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,9 @@
+package org.springframework.sfgpetclinic.services;
+
+import org.springframework.sfgpetclinic.model.Speciality;
+
+/**
+ * Created by Polik on 2/1/2022
+ */
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/org/springframework/sfgpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,38 @@
+package org.springframework.sfgpetclinic.services.map;
+
+import org.springframework.sfgpetclinic.model.Speciality;
+import org.springframework.sfgpetclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by Polik on 3/2/2022
+ */
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
