62aba392c524f31c2e2556cc462adcd4456c8773
lodjdheheu <lodjdheheu@gmail.com>
2022-04-30 15:24:14 +0300

    Updated base entity. closes #32

--- pet-clinic-data/src/main/java/org/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/org/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,15 +1,27 @@
 package org.springframework.sfgpetclinic.model;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.domain.Persistable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by Polik on 2/6/2022
  */
-@Getter @Setter @NoArgsConstructor
-public class BaseEntity implements Serializable {
+@Getter @Setter
+@MappedSuperclass
+public class BaseEntity implements Serializable, Persistable<Long> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Override
+    public boolean isNew() {
+        return id == null;
+    }
 }
