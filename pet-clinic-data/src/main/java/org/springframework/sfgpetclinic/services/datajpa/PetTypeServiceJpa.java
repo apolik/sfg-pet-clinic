b8f0f8708cfc67521f501007b3c59023d397215d
lodjdheheu <lodjdheheu@gmail.com>
2022-05-03 14:46:58 +0300

    added spring data jpa service for pet type. closes #39

---
+++ pet-clinic-data/src/main/java/org/springframework/sfgpetclinic/services/datajpa/PetTypeServiceJpa.java
@@ -0,0 +1,43 @@
+package org.springframework.sfgpetclinic.services.datajpa;
+
+import org.springframework.sfgpetclinic.model.PetType;
+import org.springframework.sfgpetclinic.repositories.PetTypeRepository;
+import org.springframework.sfgpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+/**
+ * Created by Polik on 2/1/2022
+ */
+public class PetTypeServiceJpa implements PetTypeService {
+    private final PetTypeRepository repository;
+
+    public PetTypeServiceJpa(PetTypeRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return repository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return repository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        repository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        repository.deleteById(aLong);
+    }
+}
